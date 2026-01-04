3c111c1dfdfa87011700fa193d531739997bc2b3
YoongEng <sd23048@adab.umpsa.edu.my>
2026-01-04 16:27:32 +0800

    Add user controls for input range and value selection
    Updated the ReLU app to include user controls for input range and value.

--- ReLU_app.py
+++ ReLU_app.py
@@ -3,15 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title("ReLU Activation Function")
-st.write("f(x) = max(0, x)")
+st.latex(r"f(x) = \max(0, x)")
 
-x = np.linspace(-10, 10, 400)
+# User controls
+x_min, x_max = st.slider("Select input range", -20.0, 20.0, (-10.0, 10.0))
+x_value = st.slider("Select an input value x", x_min, x_max, 0.0)
+
+x = np.linspace(x_min, x_max, 400)
 y = np.maximum(0, x)
 
 fig, ax = plt.subplots()
 ax.plot(x, y)
+ax.scatter([x_value], [max(0, x_value)], s=80)
 ax.set_xlabel("Input (x)")
 ax.set_ylabel("Output")
 ax.set_title("ReLU Function")
 
 st.pyplot(fig)
+
+st.write("For x =", x_value, ", ReLU(x) =", max(0, x_value))
